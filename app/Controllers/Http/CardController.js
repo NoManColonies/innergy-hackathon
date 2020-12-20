475a35deb80fa6b31315dc9381a574eba78f7eb7
NoManColonies <katsunari0908@gmail.com>
2020-12-20 17:41:12 +0700

    _

--- app/Controllers/Http/CardController.js
+++ app/Controllers/Http/CardController.js
@@ -8,8 +8,30 @@ const Route = use('App/Models/Route')
 
 // const makeHouseUtil = require('../../../utils/houseUtils.func')
 // const makeHistoryUtil = require('../../../utils/historyUtils.func')
+const axios = require('axios')
 const makeCarUtil = require('../../../utils/carUtils.func')
 const makeRouteUtil = require('../../../utils/routeUtils.func')
+
+const channel = {
+  '01A': 1,
+  '02A': 2,
+  '03A': 3,
+  '04A': 4,
+  '05A': 5,
+  '06A': 6,
+  '07A': 7,
+  '08A': 8,
+  '09A': 9,
+  '10A': 10,
+  '11A': 11,
+  '12A': 12,
+  '13A': 13,
+  '14A': 14,
+  '15A': 15,
+  '16A': 16
+}
+
+const API_ENDPOINT = 'http://139.59.254.197:1880'
 
 class CardController {
   async scan ({ request, response }) {
@@ -57,6 +79,22 @@ class CardController {
         .map(route => makeRouteUtil(Route).updateByID(route.bulb_id, { status: true }))
 
       await Promise.all(promises)
+
+      try {
+        await axios({
+          method: 'get',
+          url: `${API_ENDPOINT}/barrier/on?time=10`
+        })
+      } catch (e) {
+        console.log(e)
+      }
+
+      const temp = routes.toJSON().map(route => axios({
+        method: 'get',
+        url: `${API_ENDPOINT}/lamp?ch=${channel[route.bulb_id]}&time=10`
+      }))
+
+      await Promise.all(temp)
 
       return response.status(201).send({
         status: 'success',
